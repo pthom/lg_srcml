caec9e03e24d71c7002f8ed90903eb2d12498036
Pascal Thomet <pthomet@gmail.com>
2022-07-07 13:21:41 +0200

    lg_template: working version

--- autogenerate_mylib.py
+++ autogenerate_mylib.py
@@ -1,24 +1,22 @@
 import os
 
 import litgen
-from litgen.litgen_options_implot import litgen_options_implot
 
 
 THIS_DIR = os.path.dirname(__file__)
-print(f"{THIS_DIR=}")
-CPP_HEADERS_DIR = THIS_DIR + "/external/implot"
+CPP_HEADERS_DIR = THIS_DIR + "/external/mylib"
 CPP_GENERATED_PYBIND_DIR = THIS_DIR + "/bindings"
 assert os.path.isdir(CPP_HEADERS_DIR)
 assert os.path.isdir(CPP_GENERATED_PYBIND_DIR)
 
 
 def autogenerate():
-    input_cpp_header = CPP_HEADERS_DIR + "/implot.h"
-    output_cpp_pydef_file = CPP_GENERATED_PYBIND_DIR + "/pybind_lg_implot.cpp"
-    output_stub_pyi_file = CPP_GENERATED_PYBIND_DIR + "/lg_implot/__init__.pyi"
+    input_cpp_header = CPP_HEADERS_DIR + "/mylib.h"
+    output_cpp_pydef_file = CPP_GENERATED_PYBIND_DIR + "/pybind_mylib.cpp"
+    output_stub_pyi_file = CPP_GENERATED_PYBIND_DIR + "/mylib/__init__.pyi"
 
     # Configure options
-    options = litgen_options_implot()
+    options = litgen.LitgenOptions()
     generated_code = litgen.generate_code(options, filename=input_cpp_header)
 
     litgen.write_generated_code(
@@ -29,5 +27,4 @@ def autogenerate():
 
 
 if __name__ == "__main__":
-    print("autogenerate_hello_implot")
     autogenerate()
--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 
 setup(
-    name="lg-implot",
+    name="mylib",
     version="0.1.0",
     author="Pascal Thomet",
     author_email="pthomet@gmail.com",
-    description="lg-implot, bindings for implot, using litgen",
-    url="https://github.com/epezent/implot",
-    packages=(["lg_implot"]),
+    description="mylib, template bindings for litgen",
+    url="https://github.com/pthom/litgen",
+    packages=(["mylib"]),
     package_dir={"": "bindings"},
-    cmake_install_dir="bindings/lg_implot",
+    cmake_install_dir="bindings/mylib",
     include_package_data=True,
     extras_require={"test": ["pytest"]},
     python_requires=">=3.6",
-    package_data={"lg_implot": ["*.pyi"]},
-    install_requires=[
-        "lg-imgui @ git+https://github.com/pthom/lg_imgui.git",
-    ],
+    package_data={"mylib": ["*.pyi"]},
+    install_requires=[],
 )
